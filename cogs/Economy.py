acb3aff9ef594bdcba6cb8de9bff52bb097590ac
_ziyiyap <yapziyi13@gmail.com>
2023-02-19 09:31:44 +0800

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -6,14 +6,14 @@ class Economy(discord.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@discord.slash_command(name='bank', description='Checks your current balance in your bank')
-async def bank(self, ctx):
-    await ctx.respond("You are broke")
-    command_payload = {
-        "name": "bank",
-        "description": "Checks your current balance in your bank"
-        }
-    await self.http.request(discord.Route("PUT",'/applications/1076536828422782987/commands'), json=command_payload)
+    @discord.slash_command(name='bank', description='Checks your current balance in your bank')
+    async def bank(self, ctx):
+        await ctx.respond("You are broke")
+        command_payload = {
+            "name": "bank",
+            "description": "Checks your current balance in your bank"
+            }
+        await self.http.request(discord.Route("PUT",'/applications/1076536828422782987/commands'), json=command_payload)
 
 def setup(bot):
     bot.add_cog(Economy(bot))  
